9b569767366d0e52ef45c3b3df28a960ccecb843
Alex Di Battista <adibattista@nascentdigital.com>
2014-04-26 13:40:15 -0400

    Move the json file to RAW folder

--- GreenPFinder/src/com/nascent/android/glass/glasshackto/greenpfinder/JSONParser.java
+++ GreenPFinder/src/com/nascent/android/glass/glasshackto/greenpfinder/JSONParser.java
@@ -1,5 +1,12 @@
 package com.nascent.android.glass.glasshackto.greenpfinder;
 
+import java.io.InputStream;
+
+import org.json.JSONArray;
+import org.json.simple.JSONValue;
+
+import android.content.Context;
+
 public class JSONParser {
 	// http://www.oracle.com/technetwork/articles/java/json-1973242.html
 	
@@ -8,16 +15,21 @@ public class JSONParser {
 	// Or we can use Google's simple-json toolkit: 
 	// https://code.google.com/p/json-simple/wiki/DecodingExamples
 	
-	public Object readJSONFile(String filename) {
+	
+	public Object readJSONFile(Context context, String filename) {
 		
 		// TODO: load JSON file into string
-		String contents = null;
+		InputStream contents = context.getResources().openRawResource(R.raw.greenp);
 		
 		return parseJSONContents(contents);
 	}
 
 	public Object parseJSONContents(Object contents) {
 		// TODO: 
+		Object obj = JSONValue.parse(contents.toString());
+		
+		System.out.println(obj);
+		
 		return null;
 	}
 	
